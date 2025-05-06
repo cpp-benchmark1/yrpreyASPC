140d82b9ae6290762c18fa91464ec19e6c601024
cpp-benchmark1 <cpp.benchmark@proton.me>
2025-05-06 12:51:04 +0100

    CWE-121 adding the ifdef windows

--- complex_cwe121_v3.c
+++ complex_cwe121_v3.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+#ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <wchar.h>
 #include <locale.h>
-
 #pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
